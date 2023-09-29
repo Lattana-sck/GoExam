ce0294c7a3a9e3ff3caa64f547fa230f0d74b483
Lattana SCK <lattana.soucksengphet@epitech.eu>
2023-09-29 11:42:58 +0200

    [ADD] scan func to get opened ports

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "net"
+    "sync"
+    "time"
 )
 
-func main () {
-	fmt.Println("Starting...")
+const (
+    startPort = 1
+    endPort   = 65535
+    timeout   = 2 * time.Second
+)
+
+func scanPort(ip string, port int, wg *sync.WaitGroup, openPorts chan int) {
+    defer wg.Done()
+
+    address := fmt.Sprintf("%s:%d", ip, port)
+    conn, err := net.DialTimeout("tcp", address, timeout)
+
+    if err == nil {
+        conn.Close()
+        openPorts <- port
+    }
+}
+
+func main() {
+    ip := "10.49.122.144"
+    var wg sync.WaitGroup
+
+    openPorts := make(chan int)
+
+    for port := startPort; port <= endPort; port++ {
+        wg.Add(1)
+        go scanPort(ip, port, &wg, openPorts)
+    }
+
+    go func() {
+        wg.Wait()
+        close(openPorts)
+    }()
+
+    for openPort := range openPorts {
+        fmt.Printf("Port ouvert : %d\n", openPort)
+    }
 }
