ee86a6ce5aab516efe9f1a1263ba8fd4d77055ee
Lattana SCK <lattana.soucksengphet@epitech.eu>
2023-09-29 14:23:24 +0200

    [ADD] signUp & check function

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -12,65 +14,126 @@ import (
 )
 
 const (
-    startPort = 1024
-    endPort   = 65535
-    timeout   = 2 * time.Second
+	startPort = 1024
+	endPort   = 8192
+	timeout   = 2 * time.Second
+)
+
+var (
+	rightPort int
+	mutex     sync.Mutex
 )
 
 func scanPort(ip string, port int, wg *sync.WaitGroup, openPorts chan int) {
-    defer wg.Done()
+	defer wg.Done()
 
-    address := fmt.Sprintf("%s:%d", ip, port)
-    conn, err := net.DialTimeout("tcp", address, timeout)
+	address := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", address, timeout)
 
-    if err == nil {
-        conn.Close()
-        openPorts <- port
-
-        // Appeler getPing pour effectuer une requête HTTP GET
-        go getPing(ip, port)
-    }
+	if err == nil {
+		conn.Close()
+		openPorts <- port
+		go getPing(ip, port)
+	}
 }
 
 func getPing(ip string, port int) {
-    url := fmt.Sprintf("http://%s:%d/ping", ip, port)
+	url := fmt.Sprintf("http://%s:%d/ping", ip, port)
 
-    resp, err := http.Get(url)
-    if err != nil {
-        fmt.Printf("Erreur lors de la requête HTTP GET : %v\n", err)
-        return
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode == http.StatusOK {
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            return
-        }
-        fmt.Printf("Réponse de la requête HTTP GET pour le port %d : %s\n", port, string(body))
-    }
+	if resp.StatusCode == http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return
+		}
+		fmt.Printf("http://10.49.122.144:%d/ping : %s\n", port, string(body))
+
+		mutex.Lock()
+		rightPort = port
+		mutex.Unlock()
+	}
+}
+
+func signUp(ip string, port int) {
+	url := fmt.Sprintf("http://%s:%d/signup", ip, port)
+
+	data := map[string]string{"User": "Lattana"}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Erreur lors de la conversion en JSON:", err)
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Erreur lors de la requête POST:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture de la réponse POST:", err)
+		return
+	}
+
+	fmt.Printf("http://10.49.122.144:%d/signup :%s\n", port, string(responseBody))
+}
+
+func check(ip string, port int) {
+	url := fmt.Sprintf("http://%s:%d/check", ip, port)
+
+	data := map[string]string{"User": "Lattana"}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Erreur lors de la conversion en JSON:", err)
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Erreur lors de la requête POST:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture de la réponse POST:", err)
+		return
+	}
+
+	fmt.Printf("http://10.49.122.144:%d/check : %s\n", port, string(responseBody))
 }
 
 func main() {
-    ip := "10.49.122.144"
-    var wg sync.WaitGroup
+	ip := "10.49.122.144"
+	var wg sync.WaitGroup
 
-    openPorts := make(chan int)
+	openPorts := make(chan int)
 
-    for port := startPort; port <= endPort; port++ {
-        wg.Add(1)
-        go scanPort(ip, port, &wg, openPorts)
-    }
+	for port := startPort; port <= endPort; port++ {
+		wg.Add(1)
+		go scanPort(ip, port, &wg, openPorts)
+	}
 
-    go func() {
-        wg.Wait()
-        close(openPorts)
-    }()
+	go func() {
+		wg.Wait()
+		close(openPorts)
+	}()
 
-    for openPort := range openPorts {
-        fmt.Printf("Port ouvert : %d\n", openPort)
-    }
+	for openPort := range openPorts {
+		fmt.Printf("Port ouvert : %d\n", openPort)
+	}
+	go signUp(ip, rightPort)
+	go check(ip, rightPort)
 
-    app := fiber.New()
-    app.Listen(":3000") // Vous pouvez utiliser un port différent si nécessaire
+
+	app := fiber.New()
+	app.Listen(":3000")
 }
